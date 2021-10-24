06adfb89292919e3606be900a6c3b1608d9bc514
Amit <amitkr24031995@gmail.com>
2021-10-24 07:07:43 +0530

    Adding test cases

---
+++ UC1IndianStateAnalyserInformation/CensusAdapter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC1IndianStateAnalyserInformation
+{
+    public abstract class CensusAdapter
+    {
+        protected string[] GetCensusData(string csvFilePath, string dataHeaders)
+        {
+            string censusData;
+            if (!csvFilePath.Exists(csvFilePath))
+            {
+                throw new CensusAnalyserException("File Not Found", CensusAnalyserException.ExceptionType.File_NOT_FOUND);
+            }
+            if(csvFilePath.GetExtension(csvFilePath) != ".csv")
+            {
+                throw new CensusAnalyserException("Invalid File Type", CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE);
+            }
+            censusData = File.ReadAllLines(csvFilePath);
+            if (censusData[0] != dataHeaders)
+            {
+                throw new CensusAnalyserException("Incorrect header in data", CensusAnalyserException.ExceptionType.INCORRECT_HEADER);
+            }
+            return censusData;
+        }
+    }
+}
